ed8add84102894e91f7a41265cb327823be17d81
yutoVR <yuto.fushimi@icloud.com>
2019-10-14 20:41:27 +0900

    Basic loading.

--- Assets/Localizer/Scripts/ResourceBridge.cs
+++ Assets/Localizer/Scripts/ResourceBridge.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
 
 namespace yutoVR.Localizer
 {
@@ -7,13 +9,27 @@ namespace yutoVR.Localizer
 		static readonly List<IInjector> injectors = new List<IInjector>();
 		static readonly Dictionary<string, string> currentLang = new Dictionary<string, string>();
 
-		// データ構造を考える
-		// 行が項目、1列目が項目名、2列目以降は言語
-		// 2次元配列で読めばいいかな？
+		public static void Initialize()
+		{
+			// TODO どこからも呼ばずに実行できないだろうか
+			Load();
+			InjectAll();
+		}
 
-		public static void Load()
+		static void Load()
 		{
 			// TODO 言語ファイルを読み込む
+
+			var data = File.ReadAllText(""); // TODO パス指定
+			// TODO CSV Reader を TSV 用に改造できないかな。パースして二次元配列に。
+			// データ構造を考える
+			// 行が項目、1列目が項目名、2列目以降は言語
+			// 2次元配列で読めばいいかな？
+		}
+
+		public static void ChangeLanguage(string languageName)
+		{
+			// TODO 何か定数で指定したい。ランタイム Enum 的なことを
 		}
 
 		public static void AddInjector(this IInjector injector)
@@ -21,13 +37,17 @@ namespace yutoVR.Localizer
 			injectors.Add(injector);
 		}
 
-		public static void InjectAll()
+		static void InjectAll()
 		{
 			foreach (var injector in injectors)
 			{
 				if (currentLang.TryGetValue(injector.Id, out var text))
 				{
 					injector.Inject(text);
+				}
+				else
+				{
+					Debug.LogError($"ローカライズ ID: {injector.Id} は存在しません。");
 				}
 			}
 		}
