a9b2b9ac3fe2d6efedfb7790d71628da2b9d80d8
yutoVR <yuto.fushimi@icloud.com>
2019-11-03 19:41:50 +0900

    Initial implementation of data parsing.

--- Assets/Localizer/Scripts/ResourceBridge.cs
+++ Assets/Localizer/Scripts/ResourceBridge.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 namespace yutoVR.Localizer
 {
 	public static class ResourceBridge
 	{
-		static readonly List<IInjector> injectors = new List<IInjector>();
-		static readonly Dictionary<string, string> currentLang = new Dictionary<string, string>();
+		static readonly List<IInjector> Injectors = new List<IInjector>();
+		static readonly Dictionary<string, string> CurrentLang = new Dictionary<string, string>();
+		static string LastLangName;
+		static List<List<string>> Sheet;
 
 		[RuntimeInitializeOnLoadMethod]
 		public static void Initialize()
@@ -18,30 +21,42 @@ namespace yutoVR.Localizer
 
 		static void Load()
 		{
-			// TODO 言語ファイルを読み込む
-
-			var data = File.ReadAllText(""); // TODO パス指定
-			// TODO CSV Reader を TSV 用に改造できないかな。パースして二次元配列に。
-			// データ構造を考える
-			// 行が項目、1列目が項目名、2列目以降は言語
-			// 2次元配列で読めばいいかな？
+			var path = Path.Combine(Application.streamingAssetsPath, "Sample.tsv"); // TODO パス良い感じに
+			Sheet = CSVParser.LoadFromPath(path, CSVParser.Delimiter.Tab);
+			ChangeLanguage(LastLangName ?? "");
 		}
 
 		public static void ChangeLanguage(string languageName)
 		{
-			// TODO 何か定数で指定したい。ランタイム Enum 的なことを
+			var languageIndex = 1; // 最初の言語
+			if (languageName != "")
+			{
+				languageIndex = Sheet[0].Select((lang, index) => new {Index = index, Value = lang})
+				                        .First(lang => lang.Value == languageName)
+				                        .Index;
+			}
+
+			CurrentLang.Clear();
+			for (var i = 1; i < Sheet.Count; i++)
+			{
+				var key = Sheet[i][0];
+				var value = Sheet[i][languageIndex];
+				CurrentLang.Add(key, value);
+			}
+
+			LastLangName = Sheet[0][languageIndex];
 		}
 
 		public static void AddInjector(this IInjector injector)
 		{
-			injectors.Add(injector);
+			Injectors.Add(injector);
 		}
 
 		static void InjectAll()
 		{
-			foreach (var injector in injectors)
+			foreach (var injector in Injectors)
 			{
-				if (currentLang.TryGetValue(injector.Id, out var text))
+				if (CurrentLang.TryGetValue(injector.Id, out var text))
 				{
 					injector.Inject(text);
 				}
