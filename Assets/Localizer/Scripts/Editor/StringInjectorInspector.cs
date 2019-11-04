add85f410f41493ef9698da9e084b33adf75bdac
yutoVR <yuto.fushimi@icloud.com>
2019-11-04 17:00:02 +0900

    Fix a bug that prevent build.

---
+++ Assets/Localizer/Scripts/Editor/StringInjectorInspector.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using UnityEditor;
+
+namespace yutoVR.Localizer
+{
+	[CustomEditor(typeof(StringInjector))]
+	public class StringInjectorInspector : Editor
+	{
+		StringInjector injector;
+
+		void OnEnable()
+		{
+			injector = (StringInjector) target;
+		}
+
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			if (string.IsNullOrEmpty(injector.Id))
+			{
+				EditorGUILayout.HelpBox("Please enter a String ID.", MessageType.Info);
+				return;
+			}
+
+			Localizer.Load();
+			var dict = Localizer.GetDictionaryFromId(injector.Id);
+			if (dict != null)
+			{
+				var helpText = dict.Aggregate("", (current, item) => current + $"{item.Key}: {item.Value}\n");
+				helpText = helpText.TrimEnd('\n');
+				EditorGUILayout.HelpBox($"{helpText}", MessageType.Info);
+			}
+			else
+			{
+				EditorGUILayout.HelpBox($"String ID: {injector.Id} is not available.", MessageType.Error);
+			}
+		}
+	}
+}
--- Assets/Localizer/Scripts/StringInjector.cs
+++ Assets/Localizer/Scripts/StringInjector.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using TMPro;
-using UnityEditor;
+﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -33,41 +31,6 @@ namespace yutoVR.Localizer
 			else
 			{
 				Debug.LogError("No component with replaceable text.");
-			}
-		}
-	}
-
-	[CustomEditor(typeof(StringInjector))]
-	public class StringInjectorInspector : Editor
-	{
-		StringInjector injector;
-
-		void OnEnable()
-		{
-			injector = (StringInjector) target;
-		}
-
-		public override void OnInspectorGUI()
-		{
-			base.OnInspectorGUI();
-
-			if (string.IsNullOrEmpty(injector.Id))
-			{
-				EditorGUILayout.HelpBox("Please enter a String ID.", MessageType.Info);
-				return;
-			}
-
-			Localizer.Load();
-			var dict = Localizer.GetDictionaryFromId(injector.Id);
-			if (dict != null)
-			{
-				var helpText = dict.Aggregate("", (current, item) => current + $"{item.Key}: {item.Value}\n");
-				helpText = helpText.TrimEnd('\n');
-				EditorGUILayout.HelpBox($"{helpText}", MessageType.Info);
-			}
-			else
-			{
-				EditorGUILayout.HelpBox($"String ID: {injector.Id} is not available.", MessageType.Error);
 			}
 		}
 	}
