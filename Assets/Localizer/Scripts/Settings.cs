679bf4160c24f9f2e24f603fcac481d361db2b67
yutoVR <yuto.fushimi@icloud.com>
2020-02-05 00:43:44 +0900

    Settings now stored in JSON format.

--- Assets/Localizer/Scripts/Settings.cs
+++ Assets/Localizer/Scripts/Settings.cs
@@ -1,25 +1,35 @@
 ﻿using System.IO;
+using UnityEngine;
 
 namespace yutoVR.Localizer
 {
 	public static class Settings
 	{
+		class Definition
+		{
+			public string currentLanguageName;
+		}
+
 		static string SettingsDirectory => Path.Combine(Directory.GetCurrentDirectory(), "Localizer");
-		static string SettingsPath => Path.Combine(SettingsDirectory, "CurrentLanguage.txt");
+		static string SettingsPath => Path.Combine(SettingsDirectory, "Settings.json");
 
 		public static void LoadLanguageSettings()
 		{
 			if (File.Exists(SettingsPath))
 			{
-				var languageName = File.ReadAllText(SettingsPath);
-				Localizer.CurrentLanguageName = languageName;
+				var json = File.ReadAllText(SettingsPath);
+				var definition = JsonUtility.FromJson<Definition>(json);
+				Localizer.CurrentLanguageName = definition.currentLanguageName;
 			}
 		}
 
 		public static void SaveLanguageSettings()
 		{
+			var definition = new Definition { currentLanguageName = Localizer.CurrentLanguageName };
+			var json = JsonUtility.ToJson(definition, true);
+
 			Directory.CreateDirectory(SettingsDirectory);
-			File.WriteAllText(SettingsPath, Localizer.CurrentLanguageName);
+			File.WriteAllText(SettingsPath, json);
 		}
 	}
 }
