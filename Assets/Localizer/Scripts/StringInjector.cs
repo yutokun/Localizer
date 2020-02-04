ae24bd602b09543bf9d894fbfd2c1f5091c1e0a4
yutoVR <yuto.fushimi@icloud.com>
2020-02-04 23:11:00 +0900

    Fixed a bug preventing string injection.

--- Assets/Localizer/Scripts/StringInjector.cs
+++ Assets/Localizer/Scripts/StringInjector.cs
@@ -4,9 +4,33 @@ using UnityEngine.UI;
 
 namespace yutoVR.Localizer
 {
+	public enum StringType
+	{
+		TextMesh,
+		UIText,
+		TextMeshPro
+	}
+
 	public class StringInjector : MonoBehaviour, IInjector
 	{
+		[HideInInspector] public StringType stringType;
 		public string stringId;
+
+		void Reset()
+		{
+			if (GetComponent<TextMesh>())
+			{
+				stringType = StringType.TextMesh;
+			}
+			else if (GetComponent<Text>())
+			{
+				stringType = StringType.UIText;
+			}
+			else if (GetComponent<TMP_Text>())
+			{
+				stringType = StringType.TextMeshPro;
+			}
+		}
 
 		void Awake()
 		{
@@ -23,21 +47,19 @@ namespace yutoVR.Localizer
 			var text = Localizer.GetStringFromId(stringId);
 			if (text == null) Debug.LogError($"String ID: {stringId} is not available.");
 
-			if (GetComponent<TextMesh>() is TextMesh textMesh)
+			switch (stringType)
 			{
-				textMesh.text = text;
-			}
-			else if (GetComponent<TMP_Text>() is TMP_Text textMeshPro)
-			{
-				textMeshPro.text = text;
-			}
-			else if (GetComponent<Text>() is Text uiText)
-			{
-				uiText.text = text;
-			}
-			else
-			{
-				Debug.LogError("No component with replaceable text.");
+				case StringType.TextMesh:
+					GetComponent<TextMesh>().text = text;
+					break;
+
+				case StringType.UIText:
+					GetComponent<Text>().text = text;
+					break;
+
+				case StringType.TextMeshPro:
+					GetComponent<TMP_Text>().text = text;
+					break;
 			}
 		}
 
