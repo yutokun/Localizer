7658f8a65a8fdce290c68e12081b1a93421af10a
yutoVR <yuto.fushimi@icloud.com>
2020-03-04 23:45:24 +0900

    Now automatically copy LocalizedStrings.tsv to dedicated directory after build.

---
+++ Assets/Localizer/Editor/BuildPostProcess.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace yutoVR.Localizer
+{
+	public class BuildPostProcess
+	{
+		[PostProcessBuild]
+		public static void OnPostProcessBuild(BuildTarget target, string pathToBuiltProject)
+		{
+			var source = PathProvider.SheetPath;
+
+			var localizerDirectory = GetLocalizerDirectoryInBuild(pathToBuiltProject);
+			Directory.CreateDirectory(localizerDirectory);
+			var destination = Path.Combine(localizerDirectory, PathProvider.SheetName);
+
+			File.Copy(source, destination);
+		}
+
+		static string GetLocalizerDirectoryInBuild(string pathToBuiltProject)
+		{
+			var builtDir = Path.GetDirectoryName(pathToBuiltProject);
+			var dataDir = Path.GetFileNameWithoutExtension(pathToBuiltProject) + "_Data";
+			return Path.Combine(builtDir, dataDir, "Localizer");
+		}
+	}
+}
