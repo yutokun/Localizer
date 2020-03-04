0e9a10fe00508a90683864a2b21a8bb6af143dac
yutoVR <yuto.fushimi@icloud.com>
2020-03-04 23:36:21 +0900

    Add PathProvider and moved Settings.json

---
+++ Assets/Localizer/Scripts/Utils/PathProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace yutoVR.Localizer
+{
+	public class PathProvider : MonoBehaviour
+	{
+		internal static string LocalizerDirectory => Path.Combine(Application.dataPath, "Localizer");
+		static string SheetName => "LocalizedStrings.tsv";
+		static string SettingsName => "Settings.json";
+
+		internal static string SheetPath => Path.Combine(LocalizerDirectory, SheetName);
+		internal static string SettingsPath => Path.Combine(LocalizerDirectory, SettingsName);
+	}
+}
