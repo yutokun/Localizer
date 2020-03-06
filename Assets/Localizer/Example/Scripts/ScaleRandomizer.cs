00360c322c1fa64cc83a2330a59e8eca26d60219
yutoVR <yuto.fushimi@icloud.com>
2020-03-07 03:59:39 +0900

    Better Sample Scene!

---
+++ Assets/Localizer/Example/Scripts/ScaleRandomizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ScaleRandomizer : MonoBehaviour
+{
+	[SerializeField] float minScale, maxScale, interval;
+	float elapsedTime;
+	Vector3 targetScale;
+
+	void Update()
+	{
+		elapsedTime += Time.deltaTime;
+
+		if (elapsedTime > interval)
+		{
+			elapsedTime = 0f;
+			targetScale = Random.Range(minScale, maxScale) * Vector3.one;
+		}
+
+		transform.localScale = Vector3.Lerp(transform.localScale, targetScale, 0.5f);
+	}
+}
